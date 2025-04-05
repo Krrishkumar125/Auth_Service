0fba5ade50ec026c561c1ac7dee5b1384c5c33a6
Krrish Kumar <krrishkumar2028@gmail.com>
2025-04-05 15:12:07 +0530

    added jwt create and validate logic

--- src/repository/user-repository.js
+++ src/repository/user-repository.js
@@ -24,6 +24,20 @@ class UserRepository {
       throw { error };
     }
   }
+
+  async getById(userId){
+    try {
+      const user = await User.findByPk(userId , {
+        attributes: ['email' , 'id'],
+      });
+      return user;
+    } catch (error) {
+      console.log("error in user repo");
+      throw { error };
+    }
+  }
+
+  
 }
 
 module.exports = UserRepository;
